82fbb7aff9d123a21d470380264ffabf4a739b25
Dani <olivaresroza@gmail.com>
2020-12-29 20:23:01 +0100

    1.0.15

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,6 @@ class CognifitSdk {
   private getIframeStyle(): string {
     return 'position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden;';
   }
-
 }
 
 export const cognifitSdk = new CognifitSdk();
