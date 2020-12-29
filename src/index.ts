eb764d279ec755220946787b7a67b002d437de1f
Dani <olivaresroza@gmail.com>
2020-12-29 20:43:59 +0100

    insertAdjacentHTML

--- src/index.ts
+++ src/index.ts
@@ -61,12 +61,13 @@ class CognifitSdk {
   }
 
   private printIframe(type: string, key: string): void {
-    document.body.innerHTML +=
+    document.body.insertAdjacentHTML('beforeend',
       '<iframe id="cognifitSdkIframe" style="' +
       this.getIframeStyle() +
       '" title="CogniFit Access" width="100%" height="100%" src="' +
       this.cognifitSdkConfig.getIframeUrl(type, key) +
-      '" onload="window.fncIdentifierCognifitSdkRef.component.iframeUrlChanged();"></iframe>';
+      '" onload="window.fncIdentifierCognifitSdkRef.component.iframeUrlChanged();"></iframe>'
+    );
   }
 
   private getIframeStyle(): string {
